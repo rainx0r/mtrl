6885dc7f40adb67478e7d4767f186f7f15c34cfa
angel <angelos.h@hotmail.com>
2024-11-09 23:39:10 +0000

    Register FiLM

--- mtrl/nn/__init__.py
+++ mtrl/nn/__init__.py
@@ -7,6 +7,7 @@ from .care import CARENetwork
 from .multi_head import MultiHeadNetwork
 from .paco import PaCoNetwork
 from .soft_modules import SoftModularizationNetwork
+from .film import FiLMNetwork
 
 
 def get_nn_arch_for_config(
@@ -20,6 +21,8 @@ def get_nn_arch_for_config(
         return PaCoNetwork
     elif type(config) is mtrl.config.nn.CAREConfig:
         return CARENetwork
+    elif type(config) is mtrl.config.FiLMConfig:
+        return FiLMNetwork
     elif isinstance(config, mtrl.config.nn.NeuralNetworkConfig):
         return VanillaNetwork
 
