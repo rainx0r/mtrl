e426c7cb5f1d54571d17a3d2c6d2e459d364e604
rainx0r <angelos.h@hotmail.com>
2024-11-14 20:34:01 +0000

    Update experiment configurations' args

--- experiments/example.py
+++ experiments/example.py
@@ -1,29 +1,25 @@
-import jax
-import jax.numpy as jnp
 from dataclasses import dataclass
-
 from pathlib import Path
 
-from mtrl.config.optim import OptimizerConfig
-from mtrl.config.rl import OffPolicyTrainingConfig
-from mtrl.experiment import Experiment
+import tyro
+
 from mtrl.config.networks import ContinuousActionPolicyConfig, QValueFunctionConfig
 from mtrl.config.nn import MultiHeadConfig
+from mtrl.config.optim import OptimizerConfig
+from mtrl.config.rl import OffPolicyTrainingConfig
 from mtrl.envs import MetaworldConfig
+from mtrl.experiment import Experiment
 from mtrl.rl.algorithms import MTSACConfig
-
-from time import localtime, strftime
-from typing import Optional
-import tyro
 
 
 @dataclass(frozen=True)
 class Args:
-    # TODO: Make a common set of experiment args, then a set of args per alg? ie experiment args: seed, exp_name, wandb, info. Algorithm args: MTSAC: gamma, num_tasks, etc
-    seed: Optional[int] = 1
-    experiment_name: Optional[str] = None
-    wandb_project: Optional[str] = None
-    wandb_entity: Optional[str] = None
+    experiment_name: str
+    seed: int = 1
+    track: bool = False
+    wandb_project: str | None = None
+    wandb_entity: str | None = None
+    data_dir: Path = Path("./experiment_results")
 
 
 def main() -> None:
@@ -32,7 +28,7 @@ def main() -> None:
     experiment = Experiment(
         exp_name=args.experiment_name,
         seed=args.seed,
-        data_dir=Path(f"./experiment_results/{args.experiment_name}"),
+        data_dir=args.data_dir / args.experiment_name,
         env=MetaworldConfig(
             env_id="MT10",
             terminate_on_success=False,
@@ -43,30 +39,30 @@ def main() -> None:
             gamma=0.99,
             actor_config=ContinuousActionPolicyConfig(
                 network_config=MultiHeadConfig(
-                    num_tasks=10, depth=2, optimizer=OptimizerConfig(max_grad_norm=1.0)
+                    num_tasks=10, optimizer=OptimizerConfig(max_grad_norm=1.0)
                 )
             ),
             critic_config=QValueFunctionConfig(
                 network_config=MultiHeadConfig(
-                    num_tasks=10, depth=2, optimizer=OptimizerConfig(max_grad_norm=1.0)
+                    num_tasks=10, optimizer=OptimizerConfig(max_grad_norm=1.0)
                 )
             ),
             num_critics=2,
             use_task_weights=True,
         ),
         training_config=OffPolicyTrainingConfig(
-            #warmstart_steps=0,
+            warmstart_steps=0,
             total_steps=int(2e7),
             buffer_size=int(1e6),
             batch_size=1280,
-            #evaluation_frequency=1
+            # evaluation_frequency=1
         ),
         checkpoint=True,
         resume=False,
     )
 
-
-    if args.experiment_name:
+    if args.track:
+        assert args.wandb_project is not None and args.wandb_entity is not None
         experiment.enable_wandb(
             project=args.wandb_project,
             entity=args.wandb_entity,
--- experiments/mt10_moore.py
+++ experiments/mt10_moore.py
@@ -1,38 +1,35 @@
-import jax
-import jax.numpy as jnp
 from dataclasses import dataclass
-
 from pathlib import Path
 
-from mtrl.config.optim import OptimizerConfig
-from mtrl.config.rl import OffPolicyTrainingConfig
-from mtrl.experiment import Experiment
+import tyro
+
 from mtrl.config.networks import ContinuousActionPolicyConfig, QValueFunctionConfig
 from mtrl.config.nn import MOOREConfig
+from mtrl.config.optim import OptimizerConfig
+from mtrl.config.rl import OffPolicyTrainingConfig
 from mtrl.envs import MetaworldConfig
+from mtrl.experiment import Experiment
 from mtrl.rl.algorithms import MTSACConfig
-
-from time import localtime, strftime
-from typing import Optional
-import tyro
 
 
 @dataclass(frozen=True)
 class Args:
-    # TODO: Make a common set of experiment args, then a set of args per alg? ie experiment args: seed, exp_name, wandb, info. Algorithm args: MTSAC: gamma, num_tasks, etc
-    seed: Optional[int] = 1
-    experiment_name: Optional[str] = None
-    wandb_project: Optional[str] = None
-    wandb_entity: Optional[str] = None
+    seed: int = 1
+    track: bool = False
+    wandb_project: str | None = None
+    wandb_entity: str | None = None
+    data_dir: Path = Path("./experiment_results")
 
 
 def main() -> None:
     args = tyro.cli(Args)
 
+    EXPERIMENT_NAME = f"mt10_moore_{args.seed}"
+
     experiment = Experiment(
-        exp_name=args.experiment_name,
+        exp_name=EXPERIMENT_NAME,
         seed=args.seed,
-        data_dir=Path(f"./experiment_results/{args.experiment_name}"),
+        data_dir=args.data_dir / EXPERIMENT_NAME,
         env=MetaworldConfig(
             env_id="MT10",
             terminate_on_success=False,
@@ -47,7 +44,8 @@ def main() -> None:
             ),
             critic_config=QValueFunctionConfig(
                 network_config=MOOREConfig(
-                    num_tasks=10, optimizer=OptimizerConfig(max_grad_norm=1.0),
+                    num_tasks=10,
+                    optimizer=OptimizerConfig(max_grad_norm=1.0),
                 )
             ),
             num_critics=2,
@@ -62,8 +60,8 @@ def main() -> None:
         resume=False,
     )
 
-
-    if args.experiment_name:
+    if args.track:
+        assert args.wandb_project is not None and args.wandb_entity is not None
         experiment.enable_wandb(
             project=args.wandb_project,
             entity=args.wandb_entity,
