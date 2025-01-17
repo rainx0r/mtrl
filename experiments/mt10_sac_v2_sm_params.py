2d41736348ce7363f4b26ed81ebf73e095c711e4
Reginald McLean <reginald.mclean@ryerson.ca>
2025-01-17 14:41:31 -0500

    parameter matching sm

--- experiments/mt10_sac_v2_sm_params.py
+++ experiments/mt10_sac_v2_sm_params.py
@@ -26,7 +26,7 @@ def main() -> None:
     args = tyro.cli(Args)
 
     experiment = Experiment(
-        exp_name="mt10_sac_v2",
+        exp_name="mt10_sac_v2_sm_params",
         seed=args.seed,
         data_dir=args.data_dir,
         env=MetaworldConfig(
